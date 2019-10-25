faf05e89081aefb66b26da18ac28b5d7d499bd8f
shawxu <scorrxu@gmail.com>
2019-10-26 00:48:35 +0800

    加入consolePlus

---
+++ assets/js/lib/console-plus.js
@@ -0,0 +1,121 @@
+define((require) => {
+  'use strict';
+
+  const LOG_MAP = {
+      debug:    'debug'
+      , error:  'error'
+      , info:   'info'
+      , log:    'log'
+      , warn:   'warn'
+    }
+    , DEF_NAME = 'console-plus'
+    , LT_PERFORMANCE_TIME = 3
+    , SAY_HI = DEF_NAME + ' loaded, hello world!'
+    ;
+
+  let proto = {}
+    , logEntries = []
+    , logEntry = [
+        DEF_NAME //default product name
+        , '' //log level
+        , '' //absolute time
+        , '' //performance now time
+        , '' //log message
+      ]
+    , logStorage = {
+        debug:    []
+        , error:  []
+        , info:   []
+        , log:    []
+        , warn:   []
+      }
+    , clearTimes = 0
+    , reportUrlCfg = 'https://shawxu.cn:3000/' //上报结果的接口URL，可配置
+    , injected = false
+    , silent = false;
+
+  function consoleFactory(n) {
+    if (LOG_MAP[n]) {
+      return (...args) => {
+        let t;
+        logEntry[1] = n;
+        logEntry[LT_PERFORMANCE_TIME] = performance.now();
+        logEntry[4] = args.join(' ');
+
+        logEntries.push(t = logEntry.join('\t'));
+        logStorage[n].push(t);
+        !silent && !injected && console[n].call(console, logEntry[4]);
+      };
+    } else {
+      return (...args) => {
+        console[n].apply(console, args);
+      };
+    }
+  }
+
+  for (let k in console) {
+    //给console-plus补上所有console原生的方法，可以代理调用
+    if ('function' === typeof console[k]) {
+      proto[k] = consoleFactory(k);
+    }
+  }
+
+  proto.config = ({ productName = logEntry[0], reportUrl = reportUrlCfg, silentMode = false } = {}) => {
+    if (productName && 'string' === typeof productName) logEntry[0] = productName;
+    if (reportUrl && 'string' === typeof reportUrl) reportUrlCfg = reportUrl;
+    silent = !!silentMode;
+    //TODO
+  };
+
+  proto.get = filter => {
+    let r = logStorage[filter] || logEntries;
+    return r.join('\r\n');
+  };
+
+  proto.clear = clearConsole => {
+    logEntries = [];
+    for (let k in logStorage) {
+      logStorage[k] = [];
+    }
+
+    clearConsole && console.clear && console.clear();
+
+    proto.info('console-plus cleared, ', ++clearTimes, 'times');
+  };
+
+  proto.report = ({
+    componentUrl = './components/report',
+    reportUrl = reportUrlCfg,
+    filter,
+    params,
+    clear = true
+  } = {}) => {
+    ('function' === typeof require) && require([componentUrl], rpt => {
+      rpt.bootstrap({
+        'reportUrl':    reportUrl
+        , 'filter':     filter
+        , 'extParams':  params
+        , 'clear':      clear
+        , 'logStorage': logStorage
+        , 'logEntries': logEntries
+        , 'refer':      proto
+      });
+    });
+  };
+
+  proto.inject = () => {
+    if(!injected){
+      for (let k in LOG_MAP) {
+        if ('function' === typeof console[k]) {
+          console[k] = proto[k];
+        }
+      }
+      injected = true;
+    }
+  };
+
+  proto.info(SAY_HI);
+
+  return proto; //export consolePlus
+});
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,14 @@ requirejs.config({
       // the '.js' file extension.
       jquery: 'lib/jquery',
       popperjs: 'lib/popper',
+      consolePlus: 'lib/console-plus',
       //bootstrap: 'lib/bootstrap', //只使用附加popper以后的bootstrap，在requirejs体系更方便
       bootstrapBundle: 'lib/bootstrap.bundle'
   }
 });
 
-require(['jquery', 'bootstrapBundle'], function($, bts){
-  console.log('jq loaded!');
+require(['jquery', 'bootstrapBundle', 'consolePlus'], function($, bts, cp){
+  cp.inject();
+  console.log('jq and bootstrap loaded!');
+  $('div.container p')[0].innerText = cp.get();
 });
