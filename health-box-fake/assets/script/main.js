765b34bf40d4c092993a3e154de0f6d069c69449
Shaw Xu <i@shawxu.com>
2020-04-08 23:21:49 +0800

    Add files via upload
    增加计时变化能力

--- health-box-fake/assets/script/main.js
+++ health-box-fake/assets/script/main.js
@@ -19,36 +19,50 @@ require(['domReady'], (domReady) => {
 }); */
 
 ((_w, _d) => {
-  let tm = new Date();
-  let yr = tm.getFullYear();
 
-  let mh = tm.getMonth() + 1;
-  (mh < 10) && (mh = '0' + mh);
+  let [tth] = _d.querySelectorAll('#timeIconBar h2');
 
-  let dt = tm.getDate();
-  (dt < 10) && (dt = '0' + dt);
+  let [dtv] = _d.querySelectorAll('#todayValue');
 
-  let hr = tm.getHours();
-  (hr < 10) && (hr = '0' + hr);
+  let [dev] = _d.querySelectorAll('#edgeValue');
 
-  let mn = tm.getMinutes();
-  (mn < 10) && (mn = '0' + mn);
+  function timerShow(timeTitleH1, dTodayValue, dEdgeValue){
+    let tm = new Date();
+    let yr = tm.getFullYear();
+  
+    let mh = tm.getMonth() + 1;
+    (mh < 10) && (mh = '0' + mh);
+  
+    let dt = tm.getDate();
+    (dt < 10) && (dt = '0' + dt);
+  
+    let hr = tm.getHours();
+    (hr < 10) && (hr = '0' + hr);
+  
+    let mn = tm.getMinutes();
+    (mn < 10) && (mn = '0' + mn);
+  
+    let sc = tm.getSeconds();
+    (sc < 10) && (sc = '0' + sc);
+  
+    if(timeTitleH1 && timeTitleH1.innerHTML) {
+      timeTitleH1.innerHTML = `${yr}年${mh}月${dt}日<br>${hr}:${mn}:${sc}`;
+    }
+  
+    ('undefined' === typeof timerShow._counter_) && (timerShow._counter_ = 0);
 
-  let sc = tm.getSeconds();
-  (sc < 10) && (sc = '0' + sc);
+    if(timerShow._counter_ < 1) {
+      if(dTodayValue && dTodayValue.innerHTML) {
+        dTodayValue.textContent = `${mh}-${dt} ${hr}:${mn}`;
+      }
+    
+      if(dEdgeValue && dEdgeValue.innerHTML) {
+        dEdgeValue.textContent = `${mh}-${dt} 24:00`;
+      }
+    }
 
-  let [timeTitleH1] = _d.querySelectorAll('#timeIconBar h2');
-  if(timeTitleH1 && timeTitleH1.innerHTML) {
-    timeTitleH1.innerHTML = `${yr}年${mh}月${dt}日<br>${hr}:${mn}:${sc}`;
+    timerShow._counter_++;
   }
 
-  let [dTodayValue] = _d.querySelectorAll('#todayValue');
-  if(dTodayValue && dTodayValue.innerHTML) {
-    dTodayValue.textContent = `${mh}-${dt} ${hr}:${mn}`;
-  }
-
-  let [dEdgeValue] = _d.querySelectorAll('#edgeValue');
-  if(dEdgeValue && dEdgeValue.innerHTML) {
-    dEdgeValue.textContent = `${mh}-${dt} 24:00`;
-  }
+  _w.setInterval(timerShow, 1000, tth, dtv, dev);
 })(window, document);
