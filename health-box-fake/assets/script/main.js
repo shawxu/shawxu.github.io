7fad03c810b10cae4e971ffe74123d7b00914a64
shawxu <scorrxu@gmail.com>
2022-06-17 15:00:39 +0800

    全面更新样式

--- health-box-fake/assets/script/main.js
+++ health-box-fake/assets/script/main.js
@@ -20,8 +20,8 @@ require(['domReady'], (domReady) => {
 
 ((_w, _d) => {
   const SECOND = 1000;
-  let [tth, dtv, dev] = _d.querySelectorAll('#timeIconBar h2, #todayValue, #edgeValue');
-
+  //let [tth, dtv, dev] = _d.querySelectorAll('#timeIconBar h2, #todayValue, #edgeValue');
+  let [tth, phtctn, dtv, dev] = _d.querySelectorAll('#timeIconBar h2, #photoContainer, #todayValue, #edgeValue');
   //let [dtv] = _d.querySelectorAll('#todayValue');
 
   //let [dev] = _d.querySelectorAll('#edgeValue');
@@ -65,11 +65,15 @@ require(['domReady'], (domReady) => {
   }
 
   function photoFileShow(evt) {
-    let [phc, ifc, pid] = _d.querySelectorAll('#formContainer, #infoContainer, #photoId');
+    let [phc, ifc, pid, ifc1] = _d.querySelectorAll('#formContainer, #infoContainer, #photoId, #infoContainer1');
 
     pid.src = evt.target.result;
     ifc.style.display = '';
+    ifc1.style.display = '';
     phc.style.display = 'none';
+
+    _w.setInterval(timerShow, SECOND, tth, dtv, dev);
+    _w.setInterval(borderShow, SECOND * 0.4, phtctn);
   }
 
   function filePreview(evt) {
@@ -87,6 +91,17 @@ require(['domReady'], (domReady) => {
     fm && fm.addEventListener && fm.addEventListener('change', filePreview);
   }
 
+  function borderShow(photoCtn) {
+    ('undefined' === typeof borderShow._counter_) && (borderShow._counter_ = 0);
+    if(borderShow._counter_ % 2 === 0) {
+      photoCtn.style.borderRadius = "38px";
+    } else {
+      photoCtn.style.borderRadius = "8px";
+    }
+    borderShow._counter_++;
+  }
+
   initEventsBind();
-  _w.setInterval(timerShow, SECOND, tth, dtv, dev);
+  //_w.setInterval(timerShow, SECOND, tth, dtv, dev);
+  //_w.setInterval(borderShow, SECOND * 0.6, phtctn);
 })(window, document);
