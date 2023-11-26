c28ad2c1ec9eb9068fb578b05bfbddb5e519505e
shawxu <scorrxu@gmail.com>
2023-11-26 22:35:05 +0800

    小调整

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
 requirejs.config({
-  //baseUrl: 'js/lib',
+  baseUrl: 'assets/js/lib',
   paths: {
       // the left side is the module ID,
       // the right side is the path to
       // the file, relative to baseUrl.
       // Also, the path should NOT include
       // the '.js' file extension.
-      jquery: 'lib/jquery',
-      popperjs: 'lib/popper',
-      consolePlus: 'lib/console-plus',
+      jquery: 'jquery',
+      //popperjs: 'lib/popper',
+      consolePlus: 'console-plus',
       //bootstrap: 'lib/bootstrap', //只使用附加popper以后的bootstrap，在requirejs体系更方便
-      bootstrapBundle: 'lib/bootstrap.bundle'
+      bootstrapBundle: 'bootstrap.bundle'
   }
 });
 
