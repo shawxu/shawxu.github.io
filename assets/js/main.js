3d205c5924dca95f9868f6ed1c19aca602e5094d
shawxu <scorrxu@gmail.com>
2023-11-27 22:58:58 +0800

    更新到bootstrap 5.3.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,17 @@ requirejs.config({
       // the file, relative to baseUrl.
       // Also, the path should NOT include
       // the '.js' file extension.
-      jquery: 'jquery',
+      //jquery: 'jquery',
       //popperjs: 'lib/popper',
       consolePlus: 'console-plus',
       //bootstrap: 'lib/bootstrap', //只使用附加popper以后的bootstrap，在requirejs体系更方便
-      bootstrapBundle: 'bootstrap.bundle'
+      bootstrapBundle: 'https://s3.shawxu.net/js/lib/bootstrap/bootstrap.bundle.min'
   }
 });
 
-require(['jquery', 'bootstrapBundle', 'consolePlus'], function($, bts, cp){
+require([/*'jquery', */'bootstrapBundle', 'consolePlus'], function(/*$, */bts, cp){
   cp.inject();
   console.log('jq and bootstrap loaded!');
-  $('div.container p')[0].innerText = cp.get();
+  //$('div.container p')[0].innerText = cp.get();
+  document.querySelector("p#xx_main_content").innerText = cp.get();
 });
